2b5cf80359d19840a58c8f1eca5e78cc4b022f39
White Ace <whiteace@sina.com>
2020-10-11 16:22:44 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db07f70847320d18c39cb01af6aaf428'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97edb3f6-f24c-42d0-a6d8-6b8157e4df7a'
+};
