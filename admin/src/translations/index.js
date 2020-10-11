df835a4c688afab9cf5e05e37d155bfd40effe67
White Ace <whiteace@sina.com>
2020-10-12 00:37:23 +0800

    fix: tanslations

---
+++ admin/src/translations/index.js
@@ -0,0 +1,14 @@
+import en from "./en.json";
+import zhHans from "./zh-Hans.json";
+
+const trads = {
+  en,
+  "zh-Hans": zhHans,
+};
+
+export const languageNativeNames = {
+  en: "English",
+  "zh-Hans": "中文 (简体)",
+};
+
+export default trads;
